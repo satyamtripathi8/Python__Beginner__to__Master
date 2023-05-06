47919b33bceec862ebe4acad6485940a52266f26
satyamtripathi8 <satyamtripathi038@gmail.com>
2023-05-06 18:36:02 +0530

    Function

---
+++ Functions/Function_into.py
@@ -0,0 +1,8 @@
+# Creating which will return power function of given number.
+def fun(x, y):
+    return x**y
+
+a = int(input("Enter Number"))
+b = int(input("Enter Power"))
+
+print(fun(a,b))  #Calling function inside print function
